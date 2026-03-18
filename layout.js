0274567fee2293667a8bbcfd0aa209fdb81ce7fe
TharitNut <nong.peanut.butter.mail@gmail.com>
2026-03-18 15:12:31 +0700

    UI/UX Redesign: typography, glassmorphism, readability

--- layout.js
+++ layout.js
@@ -6,9 +6,9 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    toTopBtn.style.display = "block";
+    mybutton.style.display = "block";
   } else {
-    toTopBtn.style.display = "none";
+    mybutton.style.display = "none";
   }
 }
 
